e8c40613615e5e7fcffac5cc50a1846a2c07b54b
Todd Harris <bitbucket@tharris.org>
2010-07-05 15:10:40 -0400

    removed malayi temp file that is choking merges

--- purge_temporary_files.cron.sh
+++ purge_temporary_files.cron.sh
@@ -2,9 +2,9 @@
 
 # GBrowse1
 cd /usr/local/wormbase/gbrowse1/tmp
-find . -type f -atime +20 -print -exec rm -rf {} \;
+find . -type f -atime +4 -print -exec rm -rf {} \;
 
 # GBrowse2
 cd /usr/local/wormbase/gbrowse2/tmp
-find . -type f -atime +20 -print -exec rm -rf {} \;
+find . -type f -atime +4 -print -exec rm -rf {} \;
 
